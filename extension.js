7e78d2ea1396a2e4a11a3330f603c8236645ac78
Mark Lavercombe <mlava@users.noreply.github.com>
2023-10-19 14:11:58 +1100

    Update extension.js

--- extension.js
+++ extension.js
@@ -2,7 +2,6 @@ var buttonTrigger = false;
 
 export default {
     onload: ({ extensionAPI }) => {
-        var buttonUid;
 
         checkFirstRun();
 
@@ -11,28 +10,43 @@ export default {
             callback: () => numberedList()
         });
 
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Remove Numbered List",
+            callback: () => removeNumberedList()
+        });
+
         const args = {
             text: "REFRESHNUMBEREDLIST",
             help: "Refresh Numbered List",
             handler: (context) => () => {
-                buttonUid = context.targetUid;
                 buttonTrigger = context.variables.buttonTrigger;
-                numberedList(buttonTrigger, buttonUid);
+                numberedList(buttonTrigger);
+            },
+        };
+
+        const args1 = {
+            text: "REMOVENUMBEREDLIST",
+            help: "Remove Numbered List",
+            handler: (context) => () => {
+                buttonTrigger = context.variables.buttonTrigger;
+                removeNumberedList(buttonTrigger);
             },
         };
 
         if (window.roamjs?.extension?.smartblocks) {
             window.roamjs.extension.smartblocks.registerCommand(args);
+            window.roamjs.extension.smartblocks.registerCommand(args1);
         } else {
             document.body.addEventListener(
                 `roamjs:smartblocks:loaded`,
                 () =>
                     window.roamjs?.extension.smartblocks &&
-                    window.roamjs.extension.smartblocks.registerCommand(args)
+                    window.roamjs.extension.smartblocks.registerCommand(args) &&
+                    window.roamjs.extension.smartblocks.registerCommand(args1)
             );
         }
 
-        async function numberedList(buttonTrigger, buttonUid) {
+        async function numberedList(buttonTrigger) {
             var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
 
             let q = `[:find (pull ?page
@@ -43,7 +57,7 @@ export default {
              :where [?page :block/uid "${startBlock}"]  ]`;
             var info = await window.roamAlphaAPI.q(q);
 
-            var buttonString = "{{Refresh:SmartBlock:Refresh Numbered List:buttonTrigger=true,RemoveButton=false}}";
+            var buttonString = "{{Refresh Numbering:SmartBlock:Refresh Numbered List:buttonTrigger=true,RemoveButton=false}} {{Remove Numbering:SmartBlock:Remove Numbered List:buttonTrigger=true,RemoveButton=false}}";
             var sortedInfo = await sortObjectsByOrder(info[0][0].children);
             var orderCorr = 0;
 
@@ -77,43 +91,20 @@ export default {
                 var headerString = "" + numberSection + ": " + blockTextString;
                 await updateBlock(info[0][0].children[i].uid, headerString);
                 if (info[0][0].children[i].hasOwnProperty('children')) {
-                    for (var j = 0; j < info[0][0].children[i].children.length; j++) {
-                        var numberSubSection = info[0][0].children[i].children[j].order + 1;
-                        var blockTextSubString = info[0][0].children[i].children[j].string.replace(regex, "");
-                        var headerSubString = "" + numberSection + "." + numberSubSection + ": " + blockTextSubString;
-                        await updateBlock(info[0][0].children[i].children[j].uid, headerSubString);
-                        if (info[0][0].children[i].children[j].hasOwnProperty('children')) {
-                            for (var k = 0; k < info[0][0].children[i].children[j].children.length; k++) {
-                                var numberSubSubSection = info[0][0].children[i].children[j].children[k].order + 1;
-                                var blockTextSubSubString = info[0][0].children[i].children[j].children[k].string.replace(regex, "");
-                                var headerSubSubString = "" + numberSection + "." + numberSubSection + "." + numberSubSubSection + ": " + blockTextSubSubString;
-                                await updateBlock(info[0][0].children[i].children[j].children[k].uid, headerSubSubString);
-                                if (info[0][0].children[i].children[j].children[k].hasOwnProperty('children')) {
-                                    for (var l = 0; l < info[0][0].children[i].children[j].children[k].children.length; l++) {
-                                        var numberSubSubSubSection = info[0][0].children[i].children[j].children[k].children[l].order + 1;
-                                        var blockTextSubSubSubString = info[0][0].children[i].children[j].children[k].children[l].string.replace(regex, "");
-                                        var headerSubSubSubString = "" + numberSection + "." + numberSubSection + "." + numberSubSubSection + "." + numberSubSubSubSection + ": " + blockTextSubSubSubString;
-                                        await updateBlock(info[0][0].children[i].children[j].children[k].children[l].uid, headerSubSubSubString);
-                                        if (info[0][0].children[i].children[j].children[k].children[l].hasOwnProperty('children')) {
-                                            for (var m = 0; m < info[0][0].children[i].children[j].children[k].children[l].children.length; m++) {
-                                                var numberSubSubSubSubSection = info[0][0].children[i].children[j].children[k].children[l].children[m].order + 1;
-                                                var blockTextSubSubSubSubString = info[0][0].children[i].children[j].children[k].children[l].children[m].string.replace(regex, "");
-                                                var headerSubSubSubSubString = "" + numberSection + "." + numberSubSection + "." + numberSubSubSection + "." + numberSubSubSubSection + "." + numberSubSubSubSubSection + ": " + blockTextSubSubSubSubString;
-                                                await updateBlock(info[0][0].children[i].children[j].children[k].children[l].children[m].uid, headerSubSubSubSubString);
-                                                if (info[0][0].children[i].children[j].children[k].children[l].children[m].hasOwnProperty('children')) {
-                                                    for (var n = 0; n < info[0][0].children[i].children[j].children[k].children[l].children[m].children.length; n++) {
-                                                        var numberSubSubSubSubSubSection = info[0][0].children[i].children[j].children[k].children[l].children[m].children[n].order + 1;
-                                                        var blockTextSubSubSubSubSubString = info[0][0].children[i].children[j].children[k].children[l].children[m].children[n].string.replace(regex, "");
-                                                        var headerSubSubSubSubSubString = "" + numberSection + "." + numberSubSection + "." + numberSubSubSection + "." + numberSubSubSubSection + "." + numberSubSubSubSubSection + "." + numberSubSubSubSubSubSection + ": " + blockTextSubSubSubSubSubString;
-                                                        await updateBlock(info[0][0].children[i].children[j].children[k].children[l].children[m].children[n].uid, headerSubSubSubSubSubString);
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                    await loop(info[0][0].children[i], numberSection);
+                }
+            }
+
+            async function loop(blocks, headerString) {
+                for (var j = 0; j < blocks.children.length; j++) {
+                    const regex = /^\d.{0,10}: /;
+                    var numberSubSection = blocks.children[j].order + 1;
+                    var newHeader = headerString + "." + numberSubSection;
+                    var blockTextSubString = blocks.children[j].string.replace(regex, "");
+                    var headerSubString = newHeader + ": " + blockTextSubString;
+                    await updateBlock(blocks.children[j].uid, headerSubString);
+                    if (blocks.children[j].hasOwnProperty('children')) { 
+                        await loop(blocks.children[j], newHeader);
                     }
                 }
             }
@@ -121,8 +112,55 @@ export default {
             var results = await window.roamAlphaAPI.q(q);
             for (var m = 0; m < results[0][0].children.length; m++) {
                 if (results[0][0].children[m].string == buttonString) {
-                    for (var n=0; n<results[0][0].children[m].children?.length; n++) {
-                        window.roamAlphaAPI.deleteBlock({"block": { "uid": results[0][0].children[m].children[n].uid }
+                    for (var n = 0; n < results[0][0].children[m].children?.length; n++) {
+                        window.roamAlphaAPI.deleteBlock({
+                            "block": { "uid": results[0][0].children[m].children[n].uid }
+                        })
+                    }
+                }
+            }
+            return;
+        };
+
+        async function removeNumberedList(buttonTrigger) {
+            var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+
+            let q = `[:find (pull ?page
+                [:node/title :block/string :block/uid :block/heading :block/props 
+                 :entity/attrs :block/open :block/text-align :children/view-type
+                 :block/order {:block/children ...} {:block/parents ...}
+                ])
+             :where [?page :block/uid "${startBlock}"]  ]`;
+            var info = await window.roamAlphaAPI.q(q);
+
+            var buttonString = "{{Refresh Numbering:SmartBlock:Refresh Numbered List:buttonTrigger=true,RemoveButton=false}} {{Remove Numbering:SmartBlock:Remove Numbered List:buttonTrigger=true,RemoveButton=false}}";
+
+            for (var z = 0; z < info[0][0].children.length; z++) {
+                if (info[0][0].children[z].string == buttonString) {
+                    info[0][0].children.splice(z, 1);
+                    z--;
+                }
+            }
+            await loop(info[0][0].children);
+
+            async function loop(blocks) {
+                for (var i = 0; i < blocks.length; i++) {
+                    const regex = /^\d.{0,10}: /;
+                    var blockTextString = blocks[i].string.replace(regex, "");
+                    await updateBlock(blocks[i].uid, blockTextString);
+                    if (blocks[i].hasOwnProperty('children')) {
+                        await loop(blocks[i].children)
+                    }
+                }
+            }
+
+            buttonTrigger = false;
+            var results = await window.roamAlphaAPI.q(q);
+            for (var m = 0; m < results[0][0].children.length; m++) {
+                if (results[0][0].children[m].string == buttonString) {
+                    for (var n = 0; n < results[0][0].children[m].children?.length; n++) {
+                        window.roamAlphaAPI.deleteBlock({
+                            "block": { "uid": results[0][0].children[m].children[n].uid }
                         })
                     }
                 }
@@ -132,20 +170,16 @@ export default {
     },
     onunload: () => {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
-            label: 'JSON to Webhook'
+            label: 'Convert to Numbered List'
         });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Remove Numbered List'
+        });
+        if (window.roamjs?.extension?.smartblocks) {
+            window.roamjs.extension.smartblocks.unregisterCommand("REFRESHNUMBEREDLIST");
+            window.roamjs.extension.smartblocks.unregisterCommand("REMOVENUMBEREDLIST");
+        };
     }
-}
-
-async function updateBlock(blockUID, string) {
-    await window.roamAlphaAPI.updateBlock(
-        { block: { uid: blockUID, string: string.toString(), open: true } });
-}
-
-async function sortObjectsByOrder(o) {
-    return o.sort(function (a, b) {
-        return a.order - b.order;
-    });
 }
 
 async function checkFirstRun() {
@@ -165,6 +199,10 @@ async function checkFirstRun() {
         let subUID = await createBlock(string4, newUid, 3);
         let string5 = "<%REFRESHNUMBEREDLIST%>";
         await createBlock(string5, subUID, 1);
+        let string6 = "#SmartBlock Remove Numbered List";
+        let subUID1 = await createBlock(string6, newUid, 4);
+        let string7 = "<%REMOVENUMBEREDLIST%>";
+        await createBlock(string7, subUID1, 1);
         await window.roamAlphaAPI.ui.mainWindow.openPage({ page: { uid: newUid } });
     }
 }
@@ -177,4 +215,15 @@ async function createBlock(string, uid, order) {
             block: { string: string.toString(), uid: newUid }
         });
     return newUid;
+}
+
+async function updateBlock(blockUID, string) {
+    await window.roamAlphaAPI.updateBlock(
+        { block: { uid: blockUID, string: string.toString(), open: true } });
+}
+
+async function sortObjectsByOrder(o) {
+    return o.sort(function (a, b) {
+        return a.order - b.order;
+    });
 }
